a6b653785cf0a3c953a147a0b42398b6a5573fb6
Tam Dao <tamminhdaoabbas@gmail.com>
2017-02-17 11:45:19 -0600

    fix up markers

--- js/map.js
+++ js/map.js
@@ -409,7 +409,7 @@ function populateInfoWindow(marker, infowindow) {
 // of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
     var markerImage = {
-        url: 'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+        url: 'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
         '|40|_|%E2%80%A2',
         size: new google.maps.Size(21, 34),
         origin: new google.maps.Point(0, 0),
